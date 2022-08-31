636705651ec98cf89ec0184cdcac0fc5f877074d
LouJianan <397901804@qq.com>
2022-08-31 13:43:27 +0800

    xiugai

--- install_squid.sh
+++ install_squid.sh
@@ -2,9 +2,10 @@
 # 1.安装squid静默安装， -y：默认yes
 yum install -y squid
 # 2.下载并替换本地squid配置文件， -O：文件保存路径
-wget -O "/etc/squid/squid.conf" ""
+wget -O "/etc/squid/squid.conf" "https://github.com/dazhuzhu-plus/ip_proxy/releases/download/configuration/squid.conf"
 # 3.  检查参数
 squid -k parse
 # 4.  初始化缓存
 squid -z
+
 
