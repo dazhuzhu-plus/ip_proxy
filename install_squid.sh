9bfbd134ec694431418c869ef3e0028e5ab0e803
LouJianan <397901804@qq.com>
2022-09-02 09:23:20 +0800

    xiugai squid2

--- install_squid.sh
+++ install_squid.sh
@@ -3,10 +3,12 @@
 yum install -y squid
 # 2.下载并替换本地squid配置文件， -O：文件保存路径
 wget -O "/etc/squid/squid.conf" "https://github.com/dazhuzhu-plus/ip_proxy/releases/download/configuration/squid.conf"
+mkdir "/usr/lib/squid"
+yum install -y httpd
+htpasswd -c -b /usr/lib/squid/passwd admin  admin123
 # 3.  检查参数
 squid -k parse
 # 4.  初始化缓存
 squid -z
-
-
-sshpass -p woaini1314 ssh -p 10004 root@222.187.223.190 curl "https://github.com/dazhuzhu-plus/ip_proxy/releases/download/configuration/install_squid.sh" | bash'
+#启动squid
+systemctl start squid
